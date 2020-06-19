cbf0491562ef40c2b02fdff4b82e646d7108df48
karleshwer <kle@Honey-BadgerBook.local>
2020-06-19 16:57:41 -0400

    FirebaseTraining Min SDK 19 | Target SDK 29

---
+++ FirebaseTraining/app/src/main/java/com/example/firebasetraining/ui/main/MainFragment.java
@@ -0,0 +1,72 @@
+package com.example.firebasetraining.ui.main;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.firebasetraining.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainFragment extends Fragment {
+
+    private MainViewModel mViewModel;
+    TextView firebase_tv;
+    private static final String TAG = "MainActivity";
+
+    public static MainFragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View fragmentView = inflater.inflate(R.layout.main_fragment, container, false);
+
+        firebase_tv = (TextView) fragmentView.findViewById(R.id.firebaseText);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                firebase_tv.setText(value);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+
+        return fragmentView;
+    }
+
+
+}
