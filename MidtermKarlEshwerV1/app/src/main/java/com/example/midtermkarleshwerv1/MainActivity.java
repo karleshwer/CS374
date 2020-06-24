6d0c1541dae7c1d28249e12b5b700fa24551a3e3
karleshwer <kle@Honey-BadgerBook.local>
2020-06-23 20:18:56 -0400

    Fixed crash on no input

--- MidtermKarlEshwerV1/app/src/main/java/com/example/midtermkarleshwerv1/MainActivity.java
+++ MidtermKarlEshwerV1/app/src/main/java/com/example/midtermkarleshwerv1/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.midtermkarleshwerv1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 
@@ -19,14 +21,22 @@ public class MainActivity extends AppCompatActivity {
         Button buttonConvert = (Button) findViewById(R.id.button_convert);
         buttonConvert.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {;
-                EditText textBoxMi = (EditText) findViewById(R.id.editTextMiles);
-                EditText textBoxKms = (EditText) findViewById(R.id.editTextKms);
-                double miles = Double.valueOf(textBoxMi.getText().toString());
-                double kms = miles / 0.6215;
-                DecimalFormat format = new DecimalFormat("##.###");
-                textBoxKms.setText(format.format(kms));
+            public void onClick(View v) {
+                try {
+                    EditText textBoxMi = (EditText) findViewById(R.id.editTextMiles);
+                    EditText textBoxKms = (EditText) findViewById(R.id.editTextKms);
+                    double miles = Double.valueOf(textBoxMi.getText().toString());
+                    double kms = miles / 0.6215;
+                    DecimalFormat format = new DecimalFormat("##.###");
+                    textBoxKms.setText(format.format(kms));
+                }catch (Exception e) {
+                    Context context = getApplicationContext();
+                    Toast toast = Toast.makeText(context, "Please enter value", Toast.LENGTH_SHORT); toast.show();
+                    return;
+                }
+
             }
+
         });
     }
 }
